00e35f542e62764b842a6d6b0f749a1802812101
Simon Willison <swillison@gmail.com>
2020-08-22 22:50:08 -0700

    Extract some columns to separate tables
    Refs https://github.com/simonw/shapefile-to-sqlite/issues/9

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
-shapefile-to-sqlite calands.db cpad/CPAD_2020a/CPAD_2020a_Units.shp --spatial-index
-shapefile-to-sqlite calands.db cpad/CPAD_2020a/CPAD_2020a_SuperUnits.shp --spatial-index
+shapefile-to-sqlite calands.db cpad/CPAD_2020a/CPAD_2020a_Units.shp --spatial-index \
+  -c ACCESS_TYP \
+  -c AGNCY_NAME \
+  -c AGNCY_LEV \
+  -c AGNCY_TYP \
+  -c LAYER \
+  -c MNG_AGENCY \
+  -c MNG_AG_LEV \
+  -c MNG_AG_TYP \
+  -c COUNTY \
+  -c DES_TP
+shapefile-to-sqlite calands.db cpad/CPAD_2020a/CPAD_2020a_SuperUnits.shp --spatial-index \
+  -c ACCESS_TYP \
+  -c MNG_AGENCY \
+  -c MNG_AG_LEV \
+  -c MNG_AG_TYP \
+  -c LAYER
 
 sqlite-utils create-view calands.db units_with_maps_view 'select
   id,
@@ -60,6 +75,24 @@ sqlite-utils calands.db \
 sqlite-utils calands.db \
   'create table units_with_maps as select * from units_with_maps_view' \
   --load-extension=$SPATIALITE
+
+# Configure foreign keys for the materialized tables
+sqlite-utils add-foreign-key calands.db units_with_maps ACCESS_TYP
+sqlite-utils add-foreign-key calands.db units_with_maps AGNCY_NAME
+sqlite-utils add-foreign-key calands.db units_with_maps AGNCY_LEV
+sqlite-utils add-foreign-key calands.db units_with_maps AGNCY_TYP
+sqlite-utils add-foreign-key calands.db units_with_maps LAYER
+sqlite-utils add-foreign-key calands.db units_with_maps MNG_AGENCY
+sqlite-utils add-foreign-key calands.db units_with_maps MNG_AG_LEV
+sqlite-utils add-foreign-key calands.db units_with_maps MNG_AG_TYP
+sqlite-utils add-foreign-key calands.db units_with_maps COUNTY
+sqlite-utils add-foreign-key calands.db units_with_maps DES_TP
+
+sqlite-utils add-foreign-key calands.db superunits_with_maps ACCESS_TYP
+sqlite-utils add-foreign-key calands.db superunits_with_maps MNG_AGENCY
+sqlite-utils add-foreign-key calands.db superunits_with_maps MNG_AG_LEV
+sqlite-utils add-foreign-key calands.db superunits_with_maps MNG_AG_TYP
+sqlite-utils add-foreign-key calands.db superunits_with_maps LAYER
 
 # Set up full-text search
 sqlite-utils enable-fts calands.db CPAD_2020a_Units UNIT_NAME
