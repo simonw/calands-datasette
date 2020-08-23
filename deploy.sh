99de39dd80a906f5c1f16724467b0cd55ba4ef36
Simon Willison <swillison@gmail.com>
2020-08-23 13:36:53 -0700

    Deploy with datasette-graphql>=0.15
    Fixes bug with `search:` in GraphQL: https://github.com/simonw/datasette-graphql/issues/56

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ datasette publish cloudrun calands.db \
   --metadata metadata.yml \
   --memory 2Gi \
   --install datasette-leaflet-geojson>=0.6 \
-  --install datasette-graphql \
+  --install datasette-graphql>=0.15 \
   --install datasette-copyable \
   --extra-options "--config default_page_size:20"
