851eb3a9ac2196458f759d7476c45409d761159c
Simon Willison <swillison@gmail.com>
2021-01-19 21:38:02 -0800

    Updated URL

--- download.sh
+++ download.sh
@@ -2,4 +2,4 @@ mkdir -p ~/data
 conditional-get \
     --etags ~/data/etags.json \
     --output ~/data/cpad_2020a.zip \
-    'https://data.cnra.ca.gov/dataset/0ae3cd9f-0612-4572-8862-9e9a1c41e659/resource/5de6b92d-0534-422f-816e-45dbc1c48104/download/cpad_2020a.zip'
+    'https://data.cnra.ca.gov/dataset/0ae3cd9f-0612-4572-8862-9e9a1c41e659/resource/2f5efcf0-dffa-4890-92a8-3df3589f0e50/download/cpad_2020b.zip'
