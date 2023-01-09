547b8bd7864e3be2c39bbaa6ea853d2e0a635bd6
Simon Willison <swillison@gmail.com>
2023-01-09 11:33:12 -0800

    Deploy with datasette-auth-passwords, disable SQL

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ -z "$SIMON_HASH" ]; then
+    echo "Error: SIMON_HASH environment variable is not set - should contain a password hash" >&2
+    exit 1
+fi
+
 datasette publish cloudrun calands.db \
   --spatialite \
   --service calands \
@@ -8,5 +15,7 @@ datasette publish cloudrun calands.db \
   --install datasette-graphql>=0.15 \
   --install datasette-copyable \
   --install datasette-leaflet-freedraw \
+  --install datasette-auth-passwords \
   --install datasette-geojson \
-  --extra-options "--setting sql_time_limit_ms 3500 --setting default_page_size 20 --setting trace_debug 1"
+  --extra-options "--setting sql_time_limit_ms 3500 --setting default_page_size 20 --setting trace_debug 1" \
+  --plugin-secret datasette-auth-passwords simon_password_hash "$SIMON_HASH"
